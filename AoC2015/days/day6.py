efe3a646434a618f65857c6141ff7ab0f5f2d6c3
Tinynja (Dell-5290) <rantamplan1911@gmail.com>
2020-12-04 04:00:17 -0500

    2015/day6 complete

---
+++ AoC2015/days/day6.py
@@ -0,0 +1,36 @@
+from day import Day
+import numpy as np
+import re
+
+class Day6(Day):
+	def __init__(self):
+		super().__init__(__name__)
+		interpreted_data = []
+		for line in self.data.split('\n'):
+			if line.startswith('turn off'):
+				action = 0
+			elif line.startswith('turn on'):
+				action = 1
+			elif line.startswith('toggle'):
+				action = 2
+			line = re.sub(r'(turn on|turn off|toggle|through) ', '', line)
+			instruction = dict(zip(['x0', 'y0', 'x1', 'y1'], list(map(int, re.split(r'[, ]', line)))))
+			instruction['action'] = action
+			interpreted_data.append(instruction)
+		self.data = interpreted_data
+	
+	def solve1(self):
+		gridlight = np.zeros((1000,1000))
+		for inst in self.data:
+			if inst['action'] == 2:
+				gridlight[inst['x0']:inst['x1']+1, inst['y0']:inst['y1']+1] = np.logical_not(gridlight[inst['x0']:inst['x1']+1, inst['y0']:inst['y1']+1])
+			else:
+				gridlight[inst['x0']:inst['x1']+1, inst['y0']:inst['y1']+1] = inst['action']
+		self.part1 = np.count_nonzero(gridlight == 1)
+
+	def solve2(self):
+		gridlight = np.zeros((1000,1000))
+		min_0 = np.vectorize(lambda x: max(x, 0))
+		for inst in self.data:
+			gridlight[inst['x0']:inst['x1']+1, inst['y0']:inst['y1']+1] = min_0(gridlight[inst['x0']:inst['x1']+1, inst['y0']:inst['y1']+1] + [-1, 1, 2][inst['action']])
+		self.part2 = int(np.sum(gridlight))
