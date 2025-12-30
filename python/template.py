45239fc899a51639788157d197ba2560b3df7e09
Tinynja <rantamplan1911@gmail.com>
2025-12-29 21:55:21 -0500

    Create python template

---
+++ python/template.py
@@ -0,0 +1,13 @@
+# Local
+from ..solver import SolverABC
+
+
+class Solver(SolverABC):
+    def init(self):
+        ...
+
+    def solve1(self):
+        ...
+    
+    def solve2(self):
+        ...
